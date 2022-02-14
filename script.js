2f99485ceac73b4636e0240d2cce4005535c380c
Daniil <danradik01@gmail.com>
2022-02-14 17:02:44 +0300

    Изучение циклов

--- script.js
+++ script.js
@@ -1,28 +1,17 @@
 "use strict";
 
-// const num =prompt('Сколько вам лет?', '');
-// if (num < 18)
-//    document.write('Вы ещё слишком малы, для работы у нас, подождите ещё чуть-чуть)');
-// else {
-//    if (num >= 30)
-//    document.write('Мы ищем специалистов до 30, к сожалению вы нам не подходите(');
-//    else
-//       document.write('Отлично, вы нам подходите!');
+let num = 50;
+// while (num <= 55) {
+//    console.log(num);
+//    num++;
 // }
-// n = 32;
-// (n === 32) ? console.log('ok') : console.log('error');
-const num = 581;
-switch (num) {
-   case 49:
-      console.log("Неверно");
-      break;
-   case 100:
-      console.log("Неверно");
-      break;
-   case 50:
-      console.log("Верно");
-      break;
-   default:
-      console.log("Не в этот раз");
-      break;
+// do {
+//    console.log(num);
+//    num++;
+// }
+// while (num <= 55);
+for (let i = 1; i < 8; i++){
+   if (i == 4)
+   continue;
+   console.log(i);
 }
