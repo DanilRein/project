e06ac59c0657f294e3ef4cb9ed355c34a76b2b30
Daniil <danradik01@gmail.com>
2022-02-14 20:05:37 +0300

    Изучение функций

--- script.js
+++ script.js
@@ -1,17 +1,32 @@
 "use strict";
+// function declaration
+// let num = 20;
+// function showFirstMessage(ext) {
+//    console.log(ext);
+//    return(console.log(num));
+// }
+// showFirstMessage('hello world');
+// console.log(num);
+// function calc(a, b) {
+//    return (a + b);
+//    console.log('lox')
+// }
+// console.log(calc(5, 3));
+// console.log(calc(2, 81));
+// const anotherNum = ret();
+// console.log(anotherNum);
+// function ret() {
+//    let num = 50;
+//    return num;
+// }
+// function expression
+// const logger = function () {
+//    console.log('hello');
+// };
+// logger();
+//СОВРЕМЕННЫЙ ФОРМАТ
+//стрелочная функция
+const calc = (a, b) => { return (a + b) };
+console.log(calc(5, 6));
+   
 
-let num = 50;
-// while (num <= 55) {
-//    console.log(num);
-//    num++;
-// }
-// do {
-//    console.log(num);
-//    num++;
-// }
-// while (num <= 55);
-for (let i = 1; i < 8; i++){
-   if (i == 4)
-   continue;
-   console.log(i);
-}
