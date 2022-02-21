c5490195cabd4b5dc13dfa757dcda48980af54a2
Daniil <danradik01@gmail.com>
2022-02-22 00:41:39 +0300

    Знакомство с функциями callback

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
 "use strict";
-const str = 'test';
-const arr = [1, 2, 3];
-// console.log(str[2] = 'd');
-// console.log(str.toUpperCase());
-// console.log(str.toLowerCase);
-// console.log(str);
 
-// const fruit = 'Some fruit';
-// console.log(fruit.indexOf('fruit'));
+// function first() {
+//    //Do something
+//    setTimeout(function(){
+//       console.log(1);
+//    },500);
+// }
 
-// const logg = 'hello world';
-// console.log(logg.slice(6,11));
-// console.log(logg.substring(11, 6));
-// console.log(logg.substr(6, 5));
-const num = 12.4;
-console.log(Math.round(num));
-const test = '12,2px';
-console.log(parseInt());
+// function second(){
+//    console.log(2);
+// }
 
+function learnJS(lang, callback) {
+   console.log(`Я учу ${lang}`);
+   callback();
+}
+// learnJS('JavaScript', function () {
+//    console.log('Я прошёл этот урок');
+// });
+learnJS('JavaScript', done);//ВНИМАНИЕ!!! done БЕЗ КРУГЛЫХ СКОБОК
+function done() {
+   console.log('я прошёл этот урок');
+} 
+
+// first();
+// second();
