293351a04c8c90c23ed44caf4c3eb7f8b966c4e1
Daniil <danradik01@gmail.com>
2022-06-14 03:36:31 +0300

    Задачи на куб и на места в вагоне
    В первом задании измеряем площадь поверхности куба. Во втором задании мы каждому месту в вагоне находим нужный номер купе

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
 "use strict";
 
-function getTimeFromMinutes(time) {
-   if (isNaN(time) || !Number.isInteger(time) || time < 0) {
-      return (`Проверьте правильное ли число`);
+function calculateVolumeAndArea(a) {
+   if (isNaN(a) || a < 0||!Number.isInteger (a))
+      return('При вычислении произошла ошибка');
+   else {
+      let area = a * a * 6;
+      let volume = a * a * a;
+      return(`Объём куба: ${volume}, Площадь куба: ${area}`);
    }
-   let minutes = time % 60;
-   let hours = (time - minutes)/60;
-   return(`${hours} часов и ${minutes} минут`);
 }
-console.log(getTimeFromMinutes(150));
+console.log(calculateVolumeAndArea(5));
+function getCoupeNumber(a) {
+   if (isNaN(a) || Number.isInteger(a)== false || a < 0) {
+      return('Ошибка.Проверьте правильность введенного номера места');
+   }
+   if (a == 0||a>36) {
+      return('Таких мест в вагоне не существует');
+   }
+   for (let i = 4; i <= 36; i += 4)
+   {
+      if (a <= i) {
+         let b = i / 4;
+         return(b);
+         break;
+      }
+      }
+}
+console.log(getCoupeNumber(5));
+
+
