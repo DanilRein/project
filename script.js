23fe2e5b74231128d25158ad6b421757f0f937bd
Daniil <danradik01@gmail.com>
2022-06-14 18:46:01 +0300

    Числа Фибоначчи(Исправленные)
    Числа Фибоначчи без рекурсии

--- script.js
+++ script.js
@@ -3,11 +3,11 @@
 function fib(a) {
    let result = '0 1';
    let M = [0, 1];
-   if (isNaN(a) || a < 0 || a ==0) {
+   if (isNaN(a) || a < 0 || a === 0||!Number.isInteger(a)) {
       return '';
    }
    if (a == 1)
-      return 0;
+      return `0`;
    for (let i = 2; i < a; i++){
       M[i] = M[i - 1] + M[i - 2];
       result +=` ${M[i]}`;
