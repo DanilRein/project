c0f9dba98ffeb3d8e3a4ba9520f5f6326bc26934
Daniil <danradik01@gmail.com>
2022-06-15 19:01:01 +0300

    Копирование объектов и массивов
    также  их объединение

--- script.js
+++ script.js
@@ -1,31 +1,83 @@
 "use strict";
 
-const arr = [14312423, 83276, 32, 6234, 8];
-arr.sort(compareNum);
-console.log(arr);
+// let a = 5,
+//    b = a; // передача значения
 
-function compareNum(a, b) {
-   return a - b;
-} // запоминаем конструкцию, она для сортировки чисел
+// b = b + 5;
+// console.log(b);
+// console.log(a);
+// const obj = {
+//    a: 5,
+//    b: 1,
+// };
 
-// arr.pop(); // удаление в конце
+// // const copy = obj; //ссылка
+// // copy.a = 10;
+// // console.log(copy);
+// // console.log(obj);
+// function copy(mainObj) {
+// let objCopy = {};
 
-// arr.push('novoe');  //добавление в конце
-
-// console.log(arr);
-
-// for (let i = 0; i<arr.length; i++){
-//    console.log(arr[i]);
+//    for (let key in mainObj) {
+//       objCopy[key] = mainObj[key];
+//    }
+//    return objCopy;
 // }
-// for (let value of arr) {
-//    console.log(value);
+// const numbers = {
+//    a: 2,
+//    b: 5,
+//    c: {
+//       x: 7,
+//       y: 4
+//    },
+// };
+
+// const newNumbers = copy(numbers);
+
+// newNumbers.a = 10;
+// newNumbers.c.x = 10;
+
+// console.log(numbers);
+
+// console.log(newNumbers); //это всё поверхностная копия объекта
+
+// const add = {
+//    d: 17,
+//    e: 20,
+//    f: {
+//       x: 6,
+//       y: 7
+//    }
+// };
+
+// const clone = Object.assign({}, add);
+// clone.f.y = 20;
+// console.log(add);
+// console.log(clone);
+// const oldArray = ['a', 'b', 'c'];
+// const newArray = oldArray.slice();
+
+// newArray[1] = 'adadad';
+// console.log(newArray);
+// console.log(oldArray);
+
+// const video = ['youtube', 'vimeo', 'rutube'],
+//    blogs = ['wordpress', 'livejournal', 'blogger'],
+//    internet = [...video, ...blogs, 'vk', 'facebook'];
+// console.log(internet);
+// function log(a, b, c){
+//    console.log(a);
+//    console.log(b);
+//    console.log(c);
 // }
+// const num = [2, 5, 7];
+// log(...num);
+const array = ['a', 'b'];
 
-// arr[99] = 0;
+const newArray = [...array];
 
-// console.log(arr.length);
-// console.log(arr);
-// arr.forEach(function (item, i, arr) { //Ф-я для каждого элемента массива
-//    console.log(`номер по порядку: ${i}, ${item} -  внутри массива,${arr} - сам массив`);
-// });
-//  
+const q = {
+   one: 1,
+   two: 2
+};
+const newObj = {...q};
