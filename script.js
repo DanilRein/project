3a3177194c619546db1207c8da3fec490e95cd96
Daniil <danradik01@gmail.com>
2022-06-15 20:24:02 +0300

    Задания на объекты и массивы
    сначала мы создали ф-ю которая выводит сразу нам опыт человека. Затем мы создали функцию которая выводит строку с названием ЯП и уровнем его изучения. Потом добавили метод с функцией вывода возраста и языков, которыми владеет человек

--- script.js
+++ script.js
@@ -1,83 +1,36 @@
 "use strict";
 
-// let a = 5,
-//    b = a; // передача значения
-
-// b = b + 5;
-// console.log(b);
-// console.log(a);
-// const obj = {
-//    a: 5,
-//    b: 1,
-// };
-
-// // const copy = obj; //ссылка
-// // copy.a = 10;
-// // console.log(copy);
-// // console.log(obj);
-// function copy(mainObj) {
-// let objCopy = {};
-
-//    for (let key in mainObj) {
-//       objCopy[key] = mainObj[key];
-//    }
-//    return objCopy;
-// }
-// const numbers = {
-//    a: 2,
-//    b: 5,
-//    c: {
-//       x: 7,
-//       y: 4
-//    },
-// };
-
-// const newNumbers = copy(numbers);
-
-// newNumbers.a = 10;
-// newNumbers.c.x = 10;
-
-// console.log(numbers);
-
-// console.log(newNumbers); //это всё поверхностная копия объекта
-
-// const add = {
-//    d: 17,
-//    e: 20,
-//    f: {
-//       x: 6,
-//       y: 7
-//    }
-// };
-
-// const clone = Object.assign({}, add);
-// clone.f.y = 20;
-// console.log(add);
-// console.log(clone);
-// const oldArray = ['a', 'b', 'c'];
-// const newArray = oldArray.slice();
-
-// newArray[1] = 'adadad';
-// console.log(newArray);
-// console.log(oldArray);
-
-// const video = ['youtube', 'vimeo', 'rutube'],
-//    blogs = ['wordpress', 'livejournal', 'blogger'],
-//    internet = [...video, ...blogs, 'vk', 'facebook'];
-// console.log(internet);
-// function log(a, b, c){
-//    console.log(a);
-//    console.log(b);
-//    console.log(c);
-// }
-// const num = [2, 5, 7];
-// log(...num);
-const array = ['a', 'b'];
-
-const newArray = [...array];
-
-const q = {
-   one: 1,
-   two: 2
+const personalPlanPeter = {
+   name: "Peter",
+   age: "29",
+   skills: {
+      languages: ['ru', 'eng'],
+      programmingLangs: {
+         js: '20%',
+         php: '10%'
+      },
+      exp: '1 month'
+   },
+   showAgeandLangs: function (plan) {
+      let result;
+      let lang='';
+         lang=plan.skills.languages.join(' ');
+         result = `Мне ${plan.age} и я владею языками:${lang.toUpperCase()} `;
+      return result;
+   }
 };
-const newObj = {...q};
+
+function showExperience(plan) {
+
+   const {exp} = plan.skills;
+   return (exp);
+};
+
+function showProgrammingLangs(plan) {
+   let result='';
+   for (let key in plan.skills.programmingLangs) {
+      result+=(`Язык ${key} изучен на ${plan.skills.programmingLangs[key]}\n`);
+   }
+   return result;
+};
+
