ae8c8f96f7e14978965f93be6d520d2f66c19944
Daniil <danradik01@gmail.com>
2022-06-21 01:33:34 +0300

    Задание на объекты и массивы
    Создаём функцию, которая проверяет хватит ли бюджета торгового центра на его отопление. Тут различные магазины, их ширина, длина, высота, и сколько за 1 квадратный метр будет выходить денег.

--- script.js
+++ script.js
@@ -1,30 +1,38 @@
 "use strict";
 
-let str = 'some';
-let strObj = new String(str);
+const shoppingMallData = {
+   shops: [
+       {
+           width: 10,
+           length: 5
+       },
+       {
+           width: 15,
+           length: 7
+       },
+       {
+           width: 20,
+           length: 5
+       },
+       {
+           width: 8,
+           length: 10
+       }
+   ],
+   height: 5,
+   moneyPer1m3: 30,
+   budget: 50000
+}
 
-// console.log(typeof (str));
-// console.log(typeof (strObj));
-
-// console.dir([1, 2, 3]);
-
-const soldier = {
-   health: 400,
-   armor: 100,
-   sayHello: function () {
-      console.log('Hello');
-   }
-};
-
-// const john = {
-//    health: 100
-// };
-//Дальше устаревший фомат
-// john.__proto__ = soldier;
-// console.log(john.armor);
-//Дальше новые форматы
-// Object.setPrototypeOf(john, soldier)
-
-const john = Object.create(soldier);
-john.sayHello();
-// console.log(john);
+function isBudgetEnough(data) {
+   let square=0;
+   data.shops.forEach(function (item, i) {
+      square += data.shops[i].width * data.shops[i].length;
+   });
+   let price=data.height*data.moneyPer1m3*square;
+   if (price <= data.budget)
+      return 'Бюджета достаточно';
+   else
+      return 'Бюджета недостаточно';
+}
+console.log(isBudgetEnough(shoppingMallData));
