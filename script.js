8b42bfa438a0a74ace95252d35214f7ca50d25fd
Daniil <danradik01@gmail.com>
2022-06-21 15:45:03 +0300

    Задание
    есть список учеников, сортируем их и раскидываем на 3 группы, оставшихся мы записываем в отдельный элемент массива

--- script.js
+++ script.js
@@ -1,38 +1,21 @@
 "use strict";
 
-const shoppingMallData = {
-   shops: [
-       {
-           width: 10,
-           length: 5
-       },
-       {
-           width: 15,
-           length: 7
-       },
-       {
-           width: 20,
-           length: 5
-       },
-       {
-           width: 8,
-           length: 10
-       }
-   ],
-   height: 5,
-   moneyPer1m3: 30,
-   budget: 50000
-}
+const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam'];
 
-function isBudgetEnough(data) {
-   let square=0;
-   data.shops.forEach(function (item, i) {
-      square += data.shops[i].width * data.shops[i].length;
-   });
-   let price=data.height*data.moneyPer1m3*square;
-   if (price <= data.budget)
-      return 'Бюджета достаточно';
-   else
-      return 'Бюджета недостаточно';
+function sortStudentsByGroups(arr) {
+   arr.sort();
+   const a = [], b = [], c = [], rest = [];
+
+   for (let i=0; i < arr.length; i++){
+      if (i < 3) {
+         a.push(arr[i]);
+      } else if (i < 6) {
+         b.push(arr[i]);
+      } else if (i < 9) {
+         c.push(arr[i]);
+      } else if (i >= 9)
+         rest.push(arr[i]);
+   }
+   return [a, b, c, `Оставшиеся студенты: ${rest.length === 0 ? '-' : rest.join(', ')}`];
 }
-console.log(isBudgetEnough(shoppingMallData));
+console.log(sortStudentsByGroups(students));
