98e2e8a4b60c859be92c9d4bbfbd4026601c9ac1
Daniil <danradik01@gmail.com>
2022-06-24 16:44:30 +0300

    различные задания из интернета

--- script.js
+++ script.js
@@ -1,56 +1,54 @@
 "use strict";
-
-const restorantData = {
-   menu: [
-       {
-           name: 'Salad Caesar',
-           price: '14$'
-       },
-       {
-           name: 'Pizza Diavola',
-           price: '9$'
-       },
-       {
-           name: 'Beefsteak',
-           price: '17$'
-       },
-       {
-           name: 'Napoleon',
-           price: '7$'
-       }
-   ],
-   waitors: [
-       {name: 'Alice', age: 22}, {name: 'John', age: 24}
-   ],
-   averageLunchPrice: '20$',
-   openNow: true
-};
-
-function isOpen(prop) {
-   let answer = '';
-   prop ? answer = 'Открыто' : answer = 'Закрыто';
-
-   return answer;
+// возращает истинну, если хотя бы один элемент нечетный, если все четные, то false.
+function filterArr(arr) {
+    let result;
+    for (let key in arr) {
+        if (arr[key] % 2 == 0) {
+            result= false;
+        }
+        else {
+            result = true;
+            break;
+        }
+    }
+    return result;
 }
+const mass = [1,2,5];
+console.log(filterArr(mass));
 
-console.log(isOpen(restorantData.openNow));
-
-function isAverageLunchPriceTrue(fDish, sDish, average) {
-   if (+fDish.price.slice(0, -1) + (sDish.price.slice(0,-1)) < average.slice(0,-1)) {
-       return 'Цена ниже средней';
-   } else {
-       return 'Цена выше средней';
-   }
+//возращает новый массив, где все элементы кратны пяти.
+function findFive(arr) {
+    let result=[];
+    for (let key in arr)
+    {
+        if (arr[key] % 5 == 0) {
+            result.push(arr[key]);
+        }
+    }    
+    return result;
 }
+console.log(findFive(mass));
 
-
-console.log(isAverageLunchPriceTrue(restorantData.menu[0], restorantData.menu[1], restorantData.averageLunchPrice));
-
-function transferWaitors(data) {
-   const copy = Object.assign({}, data);
-
-   copy.waitors = [{name: 'Mike', age: 32}];
-   return copy.waitors[0];
+// возвращает среднее арифметическое с округлением до десятых
+function middle(arr) {
+    let result = 0;
+    arr.forEach( function(item) {
+        result += item;
+    });
+    result = result / arr.length;
+    return result.toFixed(1);
 }
+console.log(middle(mass));
 
-transferWaitors(restorantData);
+const mass2 = [1,2,5,10,23,43,12,44,34,32];
+
+//ставим первый элемент в конец массива
+function firstLast(arr) {
+    let result = [];
+    arr.forEach(function(item,i) {
+        result.push(arr[i + 1]);
+    });
+    result[arr.length-1]=arr[0];
+    return result;
+}
+console.log(firstLast(mass2));
