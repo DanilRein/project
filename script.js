543eb6fc8e92e5f8e5b182ce28077af9089ca61d
Daniil <danradik01@gmail.com>
2022-06-25 14:25:54 +0300

    работа с элементами на странице

--- script.js
+++ script.js
@@ -1,54 +1,21 @@
 "use strict";
-// возращает истинну, если хотя бы один элемент нечетный, если все четные, то false.
-function filterArr(arr) {
-    let result;
-    for (let key in arr) {
-        if (arr[key] % 2 == 0) {
-            result= false;
-        }
-        else {
-            result = true;
-            break;
-        }
-    }
-    return result;
-}
-const mass = [1,2,5];
-console.log(filterArr(mass));
 
-//возращает новый массив, где все элементы кратны пяти.
-function findFive(arr) {
-    let result=[];
-    for (let key in arr)
-    {
-        if (arr[key] % 5 == 0) {
-            result.push(arr[key]);
-        }
-    }    
-    return result;
-}
-console.log(findFive(mass));
+const box = document.getElementById('box'); //поиск по ID
 
-// возвращает среднее арифметическое с округлением до десятых
-function middle(arr) {
-    let result = 0;
-    arr.forEach( function(item) {
-        result += item;
-    });
-    result = result / arr.length;
-    return result.toFixed(1);
-}
-console.log(middle(mass));
+console.log(box);
 
-const mass2 = [1,2,5,10,23,43,12,44,34,32];
+// const btns = document.getElementsByTagName('button')[1]; //поиск по тегу(создаётся псевдомассив), а потом[нужный нам номер элемента]
 
-//ставим первый элемент в конец массива
-function firstLast(arr) {
-    let result = [];
-    arr.forEach(function(item,i) {
-        result.push(arr[i + 1]);
-    });
-    result[arr.length-1]=arr[0];
-    return result;
-}
-console.log(firstLast(mass2));
+// console.log(btns);//можно [ ] написать сюда
+// оставим одну кнопку
+const btns = document.getElementsByTagName('button');
+
+console.log(btns); // тоже будет создаваться коллекция(псевдомассив)
+
+
+const circles = document.getElementsByClassName('circle'); //по классу
+console.log(circles);
+
+const hearts = document.querySelectorAll('.heart');
+
+console.log(hearts);
