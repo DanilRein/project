e674309d6b793886f878604d2e2938c9c1c29273
Daniil <danradik01@gmail.com>
2022-07-19 21:39:10 +0300

    Продолжаем работать со страницей

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const box = document.getElementById('box'),
    btns = document.getElementsByTagName('button'),
    circles = document.getElementsByClassName('circle'),
+   wrapper = document.querySelector('.wrapper'),
    hearts = document.querySelectorAll('.heart'),
    oneHeart = document.querySelector('.heart');
 // console.dir(box);
@@ -26,3 +27,25 @@ const div = document.createElement('div');
 div.classList.add('black');
 
 document.body.append(div);
+
+// wrapper.append(div);
+// wrapper.appendChild(div);
+
+// wrapper.prepend(div);
+
+// hearts[0].before(div);
+// hearts[0].after(div);
+
+// wrapper.insertBefore(div, hearts[1]);
+
+// circles[0].remove();
+// wrapper.removeChild(hearts[1]);
+
+// hearts[0].replaceWith(circles[0]);
+// wrapper.replaceChild(circles[0],hearts[0]);//Все что с child устарели
+
+div.innerHTML = '<h3>Hello World</h3>';
+
+// div.textContent = 'Hello';
+
+div.insertAdjacentHTML('afterend','<h2>Hello</h2>');
